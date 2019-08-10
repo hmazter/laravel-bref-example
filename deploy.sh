52726635a072b8896420cc7600734800526cc4bf
Kristoffer Högberg <krihog@gmail.com>
2019-08-10 18:57:36 +0200

    Give correct permission to uploaded assets

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-aws s3 cp public/css/app.css s3://wb-laravel-bref-assets/css/app.css
-aws s3 cp public/js/app.js s3://wb-laravel-bref-assets/js/app.js
+aws s3 cp public/css/app.css s3://wb-laravel-bref-assets/css/app.css --acl=public-read
+aws s3 cp public/js/app.js s3://wb-laravel-bref-assets/js/app.js --acl=public-read
 
 serverless deploy -v
