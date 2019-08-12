6340f2e9d968decc1518f9bfc1935ab65cf2e871
Kristoffer Högberg <krihog@gmail.com>
2019-08-12 14:18:45 +0200

    Allow stages in serverless.yml, cleanup

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-aws s3 cp public/css/app.css s3://wb-laravel-bref-assets/css/app.css --acl=public-read
-aws s3 cp public/js/app.js s3://wb-laravel-bref-assets/js/app.js --acl=public-read
+# npm run production
+# composer install --no-dev
+
+aws s3 cp public/css/app.css s3://wb-laravel-bref-assets-dev/css/app.css --acl=public-read
+aws s3 cp public/js/app.js s3://wb-laravel-bref-assets-dev/js/app.js --acl=public-read
 
 serverless deploy -v
