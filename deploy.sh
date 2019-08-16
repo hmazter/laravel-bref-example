22efa55b266cf2d7731f48bd6ec18328a733458b
Kristoffer Högberg <krihog@gmail.com>
2019-08-16 20:48:28 +0200

    Sync all assets from public-folder

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # npm run production
 # composer install --no-dev
 
-aws s3 cp public/css/app.css s3://wb-laravel-bref-assets-dev/css/app.css --acl=public-read
-aws s3 cp public/js/app.js s3://wb-laravel-bref-assets-dev/js/app.js --acl=public-read
+# sync all assets from the oublic forlder to S3 (that is served by CloudFront)
+aws s3 sync public/ s3://wb-laravel-bref-assets-dev/ --acl=public-read --delete --exclude "index.php" --exclude "mix-manifest.json"
 
 serverless deploy -v
