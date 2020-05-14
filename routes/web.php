09e93fffd273c8028dbefdb65d09c2d050ac5eff
Kristoffer Högberg <krihog@gmail.com>
2020-05-14 11:00:51 +0200

    Install laravel/ui and build static assets

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('home');
 
 Route::get('queue', function () {
     dispatch(function() {
